ce4cef477febdc273f8d821bf8d398daf16c1bf9
shibayu36 <shibayu36@gmail.com>
2020-10-06 10:02:33 +0900

    12-medium-chainable-options

--- questions/12-medium-chainable-options/template.ts
+++ questions/12-medium-chainable-options/template.ts
@@ -1,4 +1,6 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable<Options = {}> = {
+  option<K extends string, V>(key: K, value: V): Chainable<Options & {
+    [k in K]: V
+  }>
+  get(): Options
 }
